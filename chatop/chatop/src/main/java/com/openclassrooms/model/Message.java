c4d3599acb4df4af65f881e78bc15afd5d1582fe
SaladinX1 <hopefulhorizon01@gmail.com>
2025-05-22 22:52:31 +0200

    Model - Définition du MessageModel

---
+++ chatop/chatop/src/main/java/com/openclassrooms/model/Message.java
@@ -0,0 +1,40 @@
+package com.openclassrooms.chatop.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "messages")
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long rentalId;
+    private Long userId;
+    private String message;
+
+    private LocalDateTime createdAt;
+
+    public Message() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    // Getters et Setters
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Long getRentalId() { return rentalId; }
+    public void setRentalId(Long rentalId) { this.rentalId = rentalId; }
+
+    public Long getUserId() { return userId; }
+    public void setUserId(Long userId) { this.userId = userId; }
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+}
