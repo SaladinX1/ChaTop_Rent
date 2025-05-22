423639a581c6fb00462ab2775db2a96d94484432
SaladinX1 <hopefulhorizon01@gmail.com>
2025-05-22 22:54:24 +0200

    DTOS - Définition du AuthResponseDto

---
+++ chatop/chatop/src/main/java/com/openclassrooms/dto/AuthResponse.java
@@ -0,0 +1,23 @@
+package com.openclassrooms.dto;
+import lombok.Data;
+//import lombok.Builder;
+
+
+@Data
+public class AuthResponse {
+    private String token;
+
+    public AuthResponse() {}
+
+    public AuthResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
