01b429a540805e7a7c48ace5d0e58ea6c3e64e91
SaladinX1 <hopefulhorizon01@gmail.com>
2025-06-05 13:46:47 +0200

    Repository - ajout du type Optional pour récupération User BDD

--- chatop/chatop/src/main/java/com/openclassrooms/repository/UserRepository.java
+++ chatop/chatop/src/main/java/com/openclassrooms/repository/UserRepository.java
@@ -1,8 +1,15 @@
 package com.openclassrooms.repository;
 
+
+ import java.util.Optional;
 import com.openclassrooms.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
+
+
+// public interface UserRepository extends JpaRepository<User, Long> {
+//     User findByEmail(String email);
+// }
