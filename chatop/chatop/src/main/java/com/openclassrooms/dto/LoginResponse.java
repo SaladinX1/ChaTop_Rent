12bc8a3835c270df5a610305070c270fe6e3aacc
SaladinX1 <hopefulhorizon01@gmail.com>
2025-06-11 23:57:32 +0200

    DTO - Mise à jour du dto loginResponse

--- chatop/chatop/src/main/java/com/openclassrooms/dto/LoginResponse.java
+++ chatop/chatop/src/main/java/com/openclassrooms/dto/LoginResponse.java
@@ -11,11 +11,20 @@ public class LoginResponse {
     @Schema(description = "Token JWT généré à l'authentification", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...")
     private String token;
 
+    @Schema(description = "Identifiant unique de l'utilisateur connecté", example = "1")
+    private Long userId;
+
     public LoginResponse() {}
 
-    public LoginResponse(String message, String token) {
+    // public LoginResponse(String message, String token) {
+    // this.message = message;
+    // this.token = token;
+    // }
+
+    public LoginResponse(String message, String token, Long userId) {
         this.message = message;
         this.token = token;
+        this.userId = userId;
     }
 
     public String getMessage() { return message; }
@@ -23,4 +32,7 @@ public class LoginResponse {
 
     public String getToken() { return token; }
     public void setToken(String token) { this.token = token; }
+
+    public Long getUserId() { return userId; }
+    public void setUserId(Long userId) { this.userId = userId; }
 }
