85206f1a4ab9857d78d323210474d28320ec505b
SaladinX1 <hopefulhorizon01@gmail.com>
2025-06-11 23:51:59 +0200

    Controller - Mise à jour controller rental

--- chatop/chatop/src/main/java/com/openclassrooms/controllers/RentalController.java
+++ chatop/chatop/src/main/java/com/openclassrooms/controllers/RentalController.java
@@ -12,8 +12,13 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+// import org.springframework.web.bind.annotation.CrossOrigin;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/rentals")
 @Tag(name = "Rental", description = "Rental management endpoints")
 public class RentalController {
