462f09e8f8a98fffa6f51e0472bad96388aae882
SaladinX1 <hopefulhorizon01@gmail.com>
2025-06-11 23:54:12 +0200

    Service - Mise à jour du service auth

--- chatop/chatop/src/main/java/com/openclassrooms/services/AuthService.java
+++ chatop/chatop/src/main/java/com/openclassrooms/services/AuthService.java
@@ -2,6 +2,7 @@ package com.openclassrooms.service;
 
 import com.openclassrooms.dto.RegisterRequest;
 import com.openclassrooms.dto.RegisterResponse;
+import com.openclassrooms.dto.LoginResponse;
 import com.openclassrooms.model.User;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import com.openclassrooms.repository.UserRepository;
@@ -12,38 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
-
-// @Service
-// public class AuthService {
-
-//     private final UserRepository userRepository;
-//     private final BCryptPasswordEncoder passwordEncoder;
-
-//     @Value("${jwt.key}")
-//     private String jwtSecret;
-
-//     public AuthService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
-//         this.userRepository = userRepository;
-//         this.passwordEncoder = passwordEncoder;
-//     }
-
-//     public RegisterResponse login(RegisterRequest request) {
-//        User user = userRepository.findByEmail(email)
-//     .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
-//         if (user == null || !passwordEncoder.matches(request.getPassword(), user.getPassword())) {
-//             throw new RuntimeException("Invalid credentials");
-//         }
-
-//         String token = Jwts.builder()
-//                 .setSubject(String.valueOf(user.getId()))
-//                 .setIssuedAt(new Date())
-//                 .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 24h
-//                 .signWith(SignatureAlgorithm.HS256, jwtSecret.getBytes())
-//                 .compact();
-
-//         return new RegisterResponse("Connexion réussie", user.getId());
-//      }
-// }
 
 @Service
 public class AuthService {
@@ -59,8 +28,8 @@ public class AuthService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    public RegisterResponse login(RegisterRequest request) {
-        String email = request.getEmail(); // récupérer email
+    public LoginResponse login(RegisterRequest request) {
+        String email = request.getEmail();
 
         User user = userRepository.findByEmail(email)
             .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
@@ -75,7 +44,9 @@ public class AuthService {
                 .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 24h
                 .signWith(SignatureAlgorithm.HS256, jwtSecret.getBytes())
                 .compact();
-
-        return new RegisterResponse("Connexion réussie", user.getId());
+   // LoginResponse response = AuthMapper.toLoginResponse("Connexion réussie", token, user.getId());
+ 
+        return new LoginResponse("Connexion réussie", token, user.getId());
     }
 }
+
